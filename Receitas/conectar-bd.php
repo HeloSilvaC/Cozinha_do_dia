b7c5f1a23fea7c96a8c9d549717edf47cb8366e6
Helo Contrera <93154949+HeloSilvaC@users.noreply.github.com>
2023-08-25 03:04:58 -0300

    Delete Receitas directory

--- Receitas/conectar-bd.php
+++
@@ -1,7 +0,0 @@
-<?php
-$host = "localhost";
-$usuario = "root";
-$senha = "";
-$banco = "receitas";
-
-$con = new mysqli($host, $usuario, $senha, $banco);
--- Receitas/valida-captcha.php
+++
@@ -1,9 +0,0 @@
-<?php
-   session_start();
-    if ($_POST["captcha"] == $_SESSION["palavra"]){
-        echo "<h1>Voce Acertou</h1>";
-    }else{
-        echo "<h1>Voce nao acertou!</h1>";
-        echo "<a href='entrar.php'></a>";
-    }
-?>
